91d2d116d10a8df695befbec7fa410f1df72522c
William Pallot <williampallot@outlook.comgit config --global user.email williampallot@outlook.com>
2020-09-13 10:35:29 +1000

    Navbar component

---
+++ src/pages/blog.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+
+const blog = () => {
+    return (
+        <div>
+            <h1>This is our blog page</h1>
+            <Link to="/">Home Page</Link>
+        </div>
+    );
+};
+
+export default blog;
